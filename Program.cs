dfed4306654129a165d42ea023133c2d10162d87
RoyaRashidi <RoyaRashidi@DESKTOP-GSHJAG6>
2025-05-28 00:07:22 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Calculator
+{
+    using System;
+
+    class Calculator
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator (+, -, *, /):");
+            char op = Convert.ToChar(Console.ReadLine());
+
+                    Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Cannot divide by zero.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    return;
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
